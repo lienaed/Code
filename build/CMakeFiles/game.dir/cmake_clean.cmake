52ceba2d735187055c6359d8980e7315e436350e
lienaed <lienaedf@gmail.com>
2025-03-26 08:41:40 -0400

    1

---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/1.cpp.o"
+  "CMakeFiles/game.dir/1.cpp.o.d"
+  "CMakeFiles/game.dir/Game.cpp.o"
+  "CMakeFiles/game.dir/Game.cpp.o.d"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
